a8801dd24622aa6eb3bdaf24d22d00b288d87834
Your Name <you@example.com>
2022-09-05 17:01:43 +0530

    project-1

---
+++ projectblog/routes/route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Router = express.Router();
+
+const AuthorController = require("../controllers/AuthorController")
+const BlogController = require("../controllers/BlogController")
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Router
+
+
+
+
+
